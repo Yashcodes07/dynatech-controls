896aea02e1dd842bfe4607e2e3925b12064af2ec
Yashcodes07 <Yashk40491@gmail.com>
2026-01-05 16:35:18 +0530

    Fix partner logo container height

--- components/Partnerslider/page.tsx
+++ components/Partnerslider/page.tsx
@@ -40,7 +40,6 @@ export default function PartnerSlider() {
                 key={`${partner.name}-${index}`} 
                 className="flex w-[175px] items-center justify-center transition-all duration-hover:opacity-100 hover:scale-110"
               >
-                // partner slider height fix
               <div className="relative h-24 w-full flex items-center justify-center px-4">
                   <Image
                     src={partner.src}
